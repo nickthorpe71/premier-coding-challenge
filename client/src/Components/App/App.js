540bdcf11a74a182d19c88f9e80421ab3aaab9d8
nickthorpe71 <nickthorpe71@gmail.com>
2020-10-21 08:22:29 -0500

    adds parent directory

---
+++ client/src/Components/App/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+// import Waypoint from '../Waypoint/Waypoint';
+import GmapsService from '../../Services/gmaps-service';
+
+import './App.css';
+
+const App = () => {
+  const [results, setResults] = useState('');
+
+  const onSubmitForm = (data) => {
+    console.log(data);
+    const wps = [];
+
+    for (const key in data)
+      if (key !== 'office')
+        wps.push(data[key]);
+
+    GmapsService.getRoute(data.office, wps)
+      .then(data => {
+        console.log(data);
+      });
+    // setResults(data);
+  };
+
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Route Finder
+      </header>
+      <main className="App-body">
+        <form className="address-form" onSubmit={handleSubmit(onSubmitForm)}>
+          <label htmlFor="office" />
+          <input type="text" id="office" name="office" required ref={register} />
+          <label htmlFor="address1" />
+          <input type="text" name="address1" ref={register} />
+          <label htmlFor="address2" />
+          <input type="text" name="address2" ref={register} />
+          <label htmlFor="address3" />
+          <input type="text" name="address3" ref={register} />
+          <label htmlFor="address4" />
+          <input type="text" name="address4" ref={register} />
+          <label htmlFor="address5" />
+          <input type="text" name="address5" ref={register} />
+          <label htmlFor="address6" />
+          <input type="text" name="address6" ref={register} />
+          <input type="submit" value="Find" />
+        </form>
+        <ol className="results">
+          {results}
+        </ol>
+      </main>
+    </div>
+  );
+};
+
+export default App;
